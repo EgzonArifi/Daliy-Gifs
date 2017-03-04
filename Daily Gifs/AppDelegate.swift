3dec8d8492d955e2e8173a107387e61f052d83fd
Egzon's Macbook <egzonarifi@live.com>
2017-03-04 20:20:57 +0100

    added files

---
+++ Daily Gifs/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Daily Gifs
+//
+//  Created by EgzonArifi on 3/4/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ Daily Gifs/FirstViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FirstViewController.swift
+//  Daily Gifs
+//
+//  Created by EgzonArifi on 3/4/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }  
+}
+
