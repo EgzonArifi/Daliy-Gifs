6cfa21683aaaee4d51884141a03e2dbf6bc30585
Egzon's Macbook <egzonarifi@live.com>
2017-03-05 18:35:39 +0100

    created constant file

---
+++ Daily Gifs/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Daily Gifs
+//
+//  Created by EgzonArifi on 3/5/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import Foundation
+
+let HOME_URL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC"
