b1826c441234e57e5e0aa768936ee91a724e7cf1
Egzon's Macbook <egzonarifi@live.com>
2017-04-05 19:50:40 +0200

    created wishlist view model

---
+++ Daily Gifs/WishlistViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  WishlistViewModel.swift
+//  Daily Gifs
+//
+//  Created by EgzonArifi on 3/19/17.
+//  Copyright © 2017 Overjump. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class WishlistViewModel: NSObject {
+
+    func saveGifInWishlist(completion: @escaping (Bool) -> Void) {
+        
+    }
+
+
+}
